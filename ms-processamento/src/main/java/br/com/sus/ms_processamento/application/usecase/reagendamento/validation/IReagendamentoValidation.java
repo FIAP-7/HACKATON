235bc592993fb8fa4edc87e7c274b0427a09b7b5
Jeferson <jeferson.herculano@outlook.com>
2026-02-10 19:58:40 -0300

    fix: Remoção fila e reagendamento

--- ms-processamento/src/main/java/br/com/sus/ms_processamento/application/usecase/reagendamento/validation/IReagendamentoValidation.java
+++
@@ -1,8 +0,0 @@
-package br.com.sus.ms_processamento.application.usecase.reagendamento.validation;
-
-import br.com.sus.ms_processamento.domain.model.Reagendamento;
-
-public interface IReagendamentoValidation {
-
-    void validate(Reagendamento reagendamento);
-}
--- ms-processamento/src/main/java/br/com/sus/ms_processamento/domain/model/StatusReagendamentoEnum.java
+++
@@ -1,8 +0,0 @@
-package br.com.sus.ms_processamento.domain.model;
-
-public enum StatusReagendamentoEnum {
-    PENDENTE,
-    PENDENTE_RESPOSTA,
-    CONFIRMADO,
-    RECUSADO
-}
